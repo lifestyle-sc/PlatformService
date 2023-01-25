ca63e0e2c9af3768b3005b3736b49e4fd1edcd99
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-25 22:51:07 +0100

    added config to main project

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlatformService.Models
+{
+    public class Platform
+    {
+        [Column("PlatformId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Name of platform is a required field.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Publisher of platform is a requried field.")]
+        public string? Publisher { get; set; }
+
+        [Required(ErrorMessage = "Cost of platform is a required field.")]
+        public string? Cost { get; set; }  
+    }
+}
