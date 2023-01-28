1344ce83cd6c695c141ddb8c3fd32f6f09eec415
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-28 23:01:32 +0100

    created prepDb class for added data to our in memory db

---
+++ PlatformService/Data/PrepDB.cs
@@ -0,0 +1,38 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDB
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using(var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+#pragma warning disable CS8604 // Possible null reference argument.
+                SeedData(context);
+#pragma warning restore CS8604 // Possible null reference argument.
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> No seeded data");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free"},
+                    new Platform() { Name = "Sql Server", Publisher = "Microsoft", Cost = "Free"},
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud native computation foundation", Cost = "Free"}
+                    );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> Seeded data available");
+            }
+        }
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -30,4 +30,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+PrepDB.PrepPopulation(app);
+
 app.Run();
