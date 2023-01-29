f6c006ee16758edd9ff08dd172fe888e1a6cb967
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2023-01-29 01:19:47 +0100

    created platform controller

---
+++ PlatformService/Controllers/PlatformController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/platforms")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
